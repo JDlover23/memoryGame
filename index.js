e3697757e909fd8d9c1688c30e8752c229341fc9
Мария Чунихина <mariacunihina@Marias-MacBook-Air.local>
2024-10-13 00:27:30 +0300

    game game boy insinuation

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-let started = false;
 let currentSequence = [];
 let userSequence = [];
 let points = 0;
 let level = 1;
-const tl = document.querySelector(".tl");
-const tr = document.querySelector(".tr");
-const bl = document.querySelector(".bl");
-const br = document.querySelector(".br");
+
+const startScreen = document.querySelector(".start-screen");
+const resultContainer = document.querySelector(".result-container");
+const result = document.querySelector(".result");
+const content = document.querySelector(".content");
+const t = document.querySelector(".t");
+const cl = document.querySelector(".cl");
+const cr = document.querySelector(".cr");
+const b = document.querySelector(".b");
+const counter = document.querySelector(".counter");
+const info = document.querySelector(".info");
 const levelEl = document.querySelector(".level");
 const pointsEl = document.querySelector(".points");
+const startBtn = document.querySelector(".startBtn");
+const tryAgainBtn = document.querySelector(".try-again");
 const sound1 = new Audio("1.wav");
 const sound2 = new Audio("2.wav");
 const sound3 = new Audio("3.wav");
 const sound4 = new Audio("4.wav");
 
 const buttons = [
-  { el: tl, sound: sound1 },
-  { el: tr, sound: sound2 },
-  { el: bl, sound: sound3 },
-  { el: br, sound: sound4 },
+  { el: t, sound: sound1, key: "w", key2: "ArrowUp" },
+  { el: cl, sound: sound2, key: "a", key2: "ArrowLeft" },
+  { el: cr, sound: sound3, key: "d", key2: "ArrowRight" },
+  { el: b, sound: sound4, key: "s", key2: "ArrowDown" },
 ];
-pointsEl.innerHTML = `Ваши баллы: ${points}`;
-levelEl.innerHTML = `Уровень: ${level}`;
-const centerButton = document.querySelector(".center");
+pointsEl.innerHTML = `POINTS ${points}`;
+levelEl.innerHTML = `LEVEL ${level}`;
 
 function buttonsHeight() {
   buttons.forEach((btn) => {
@@ -30,29 +37,68 @@ function buttonsHeight() {
     console.log(btn.el.clientWidth);
   });
 }
-function checkStarted(started) {
-  if (started === true) {
-    centerButton.disabled = true;
-    tl.disabled = false;
-    tr.disabled = false;
-    bl.disabled = false;
-    br.disabled = false;
-  } else {
-    centerButton.disabled = false;
-    tl.disabled = true;
-    tr.disabled = true;
-    bl.disabled = true;
-    br.disabled = true;
-  }
+function counterEnable() {
+  setTimeout(() => {
+    counter.innerHTML = 2;
+  }, 1000);
+  setTimeout(() => {
+    counter.innerHTML = 1;
+  }, 2000);
+  setTimeout(() => {
+    counter.innerHTML = "";
+  }, 3000);
 }
 function levelMod() {
   if (level < 10) {
     return level * 0.2;
-  }
-  else {
-    return 2
+  } else {
+    return 2;
   }
 }
+
+function initStats() {
+  startScreen.style.display = "none";
+  resultContainer.style.display = "none";
+  content.style.opacity = "1";
+  info.style.display = "block";
+}
+function initTimeouts() {
+  setTimeout(() => {
+    counter.innerHTML = "";
+    levelEl.innerHTML = `LEVEL 1`;
+  }, 3000);
+  setTimeout(() => {
+    if (currentSequence.length < 1) {
+      randomise();
+      buttons.forEach((btn) => {
+        btn.el.disabled = false;
+      });
+    }
+  }, 3300);
+}
+startBtn.addEventListener(
+  "click",
+  function () {
+    counterEnable();
+    initStats();
+    initTimeouts();
+  },
+  false
+);
+tryAgainBtn.addEventListener(
+  "click",
+  function () {
+    counterEnable();
+    initStats();
+    initTimeouts();
+  },
+  false
+);
+
+document.addEventListener("click", writeUserSequence, false);
+document.addEventListener("keydown", checkForKeys, false);
+document.addEventListener("click", writeUserSequence, false);
+
 function randomise() {
   const number = Math.floor(Math.random() * 4);
   currentSequence.push(buttons[number].el);
@@ -68,67 +114,83 @@ function randomise() {
       }, (i * 1000) / (1 + levelMod()));
     })(i);
   }
-
   console.log(currentSequence, "currentSequence");
 }
 
-function start() {
-  checkStarted(false);
-  centerButton.addEventListener(
-    "click",
-    function () {
-      levelEl.innerHTML = `Уровень: 1`;
-      if (currentSequence.length < 1) {
-        randomise();
-        checkStarted(true);
-      }
-    },
-    false
-  );
-  function checkIfRight() {
-    if (
-      userSequence[userSequence.length - 1] ===
-      currentSequence[userSequence.length - 1]
-    ) {
-      points += 10;
-      if (userSequence.length === currentSequence.length) {
-        userSequence = [];
-        if (currentSequence.length % 5 === 0) {
-          level = currentSequence.length / 5 + 1;
-          points += 90 * (level - 1);
-        }
-        setTimeout(() => {
-          randomise();
-        }, 1000);
-      }
-    } else {
-      alert(`Вы проиграли. Ваши баллы: ${points}`);
-      userSequence = [];
-      currentSequence = [];
-      points = 0;
-      level = 1;
-      checkStarted(false);
-    }
+function writeUserSequence(btn) {
+  if (buttons.some((x) => x.el === btn.target)) {
+    userSequence.push(btn.target);
+    buttons.find((x) => x.el === btn.target).sound.play();
+    checkIfRight();
+    pointsEl.innerHTML = `POINTS ${points}`;
+    levelEl.innerHTML = `LEVEL ${level}`;
   }
-  document.addEventListener(
-    "click",
-    function (btn) {
-      if (buttons.some((x) => x.el === btn.target)) {
-        userSequence.push(btn.target);
-        buttons.find((x) => x.el === btn.target).sound.play();
-        checkIfRight();
-        pointsEl.innerHTML = `Ваши баллы: ${points}`;
-        levelEl.innerHTML = `Уровень: ${level}`;
+}
+
+function checkForKeys(e) {
+  console.log(e.key);
+  if (
+    buttons.some(
+      (x) => (x.key === e.key || x.key2 === e.key) & (x.el.disabled === false)
+    )
+  ) {
+    let myLovelyButton = buttons.find(
+      (x) => x.key === e.key || x.key2 === e.key
+    );
+    userSequence.push(myLovelyButton.el);
+    myLovelyButton.sound.play();
+    myLovelyButton.el.classList.add("active");
+    setTimeout(() => {
+      myLovelyButton.el.classList.remove("active");
+    }, 100);
+    checkIfRight();
+    pointsEl.innerHTML = `POINTS ${points}`;
+    levelEl.innerHTML = `LEVEL ${level}`;
+    console.log(userSequence);
+  }
+}
+
+function checkIfRight() {
+  if (
+    userSequence[userSequence.length - 1] ===
+    currentSequence[userSequence.length - 1]
+  ) {
+    points += 10;
+    if (userSequence.length === currentSequence.length) {
+      userSequence = [];
+      if (currentSequence.length % 5 === 0) {
+        level = currentSequence.length / 5 + 1;
+        points += 90 * (level - 1);
       }
-    },
-    false
-  );
+      setTimeout(() => {
+        randomise();
+      }, 1000);
+    }
+  } else {
+    content.style.opacity = "0";
+    info.style.display = "none";
+    resultContainer.style.display = "flex";
+    console.log(level);
+    if (level < 2) {
+      result.innerHTML = `You lose. Level ${level}. Points ${points}`;
+    } else if (1 < level < 4) {
+      result.innerHTML = `Not bad. Level ${level}. Points ${points}`;
+    } else if (4 < level < 8) {
+      result.innerHTML = `Brilliant. Level ${level}. Points ${points}`;
+    } else {
+      result.innerHTML = `Unique. Level ${level}. Points ${points}`;
+    }
+    userSequence = [];
+    currentSequence = [];
+    points = 0;
+    level = 1;
+    counter.innerHTML = 3;
+  }
 }
 
 window.addEventListener(
   "load",
   function () {
-    start();
     buttonsHeight();
   },
   false
